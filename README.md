205a0b1762c1875cdb54ce0505df7c2bfa0b43df
Rafael Lima <contato@rafael.adm.br>
2012-07-05 22:50:17 -0300

    improves the usage example

--- README.md
+++ README.md
@@ -20,7 +20,25 @@ BankBillet.password = 'X'
 ## Uso
 
 ```python
+
+# criar um boleto
+amount = 230.50
+expire_at = '2015-07-22'
+name = 'Rafael Lima'
+bank_billet = BankBillet.create({'amount':amount,'expire_at':expire_at,'name':name})
+
+# listar todos os boletos
 bank_billets = BankBillet.find()
+if not bank_billets:
+  print "Nenhum boleto bancário"
+else:
+  for bank_billet in bank_billets:
+    print "Nosso Número: %s" % bank_billet.our_number
+    print "Vencimento: %s" % bank_billet.expire_at
+    print "Valor: %s" % bank_billet.amount
+    print "Sacado: %s" % bank_billet.name
+    print "URL: %s" % bank_billet.external_link
+    print "================================="
 ```
 
 Veja um exemplo no arquivo [example.py](https://github.com/BielSystems/cobregratis-python/blob/master/example.py)
--- example.py
+++ example.py
@@ -6,16 +6,25 @@ from pyactiveresource.connection import UnauthorizedAccess
 import sys
 
 def main():
-  BankBillet.user = '3NIAl4KWk87sFBbpqOUCSURED4zIx3Xiqz8gXSDtFF77YIADbRnoCXRjCHQ3'
+  BankBillet.user = 'hcIVKwZiNwRazGg90yc4H952qNGxLtxFVBRp95Ex2CtzdjbQS4E7vFNkzBGA'
   BankBillet.password = 'X'
   try:
+    amount = 230.50
+    expire_at = '2015-07-22'
+    name = 'Rafael Lima'
+    bank_billet = BankBillet.create({'amount':amount,'expire_at':expire_at,'name':name})
+
     bank_billets = BankBillet.find()
-    for bank_billet in bank_billets:
-      print "Nosso Número: %s" % bank_billet.our_number
-      print "Vencimento: %s" % bank_billet.expire_at
-      print "Valor: %s" % bank_billet.amount
-      print "Sacado: %s" % bank_billet.name
-      print "================================="
+    if not bank_billets:
+      print "Nenhum boleto bancário"
+    else:
+      for bank_billet in bank_billets:
+        print "Nosso Número: %s" % bank_billet.our_number
+        print "Vencimento: %s" % bank_billet.expire_at
+        print "Valor: %s" % bank_billet.amount
+        print "Sacado: %s" % bank_billet.name
+        print "URL: %s" % bank_billet.external_link
+        print "================================="
   except UnauthorizedAccess:
     print "Access Denied"
 
